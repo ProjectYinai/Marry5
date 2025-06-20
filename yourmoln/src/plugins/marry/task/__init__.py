6afdb702170bfbfefeea8504cd655602203ffa39
yourmoln <yourmoln@outlook.com>
2025-06-20 15:58:04 +0800

    feat:自动同意好友请求与群聊邀请

--- yourmoln/src/plugins/marry/task/__init__.py
+++ yourmoln/src/plugins/marry/task/__init__.py
@@ -1,8 +1,25 @@
-from nonebot import require,get_bot
-
+from nonebot import require,get_bot, on_request
+from nonebot.adapters.onebot.v11 import PrivateMessageEvent, GroupMessageEvent, RequestEvent
+from nonebot.matcher import Matcher
+from nonebot.adapters import Bot
 require("nonebot_plugin_apscheduler")
 
 from nonebot_plugin_apscheduler import scheduler
+
+teaRequest=on_request(priority=99,block=False)
+@teaRequest.handle()
+async def teaRequestFun(bot: Bot, e:RequestEvent, matcher: Matcher):
+    request_type=str(e.request_type)
+    if request_type=="friend":
+        print("加好友申请٩( 'ω' )و get！")
+        flag_a=str(e.flag)
+        await bot.set_friend_add_request(flag=flag_a,approve=True)
+    elif request_type=="group":
+        print("邀请加入群٩( 'ω' )و get！")
+        sub_type=str(e.sub_type)
+        if sub_type=="invite":
+            flag_a=str(e.flag)
+            await bot.set_group_add_request(flag=flag_a,approve=True)
 
 @scheduler.scheduled_job("cron", hour="22",minute="55",second="0", id="job_4")
 async def run_every_4_day():
